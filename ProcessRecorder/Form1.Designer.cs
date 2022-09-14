3b854ef97a39cfd7ddeb4726704a53abb5f1d175
YHFENG <YHFENG@nuvoton.com>
2022-09-14 18:00:18 +0800

    Test push new project

---
+++ ProcessRecorder/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace ProcessRecorder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.ToolStripMenuItem01 = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = global::ProcessRecorder.Properties.Resources.Pen;
+            this.notifyIcon1.Text = "ProcessRecorder";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItem01});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(99, 26);
+            // 
+            // ToolStripMenuItem01
+            // 
+            this.ToolStripMenuItem01.Name = "ToolStripMenuItem01";
+            this.ToolStripMenuItem01.Size = new System.Drawing.Size(180, 22);
+            this.ToolStripMenuItem01.Text = "關閉";
+            this.ToolStripMenuItem01.Click += new System.EventHandler(this.ToolStripMenuItem01_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 161);
+            this.Icon = global::ProcessRecorder.Properties.Resources.Pen;
+            this.Name = "Form1";
+            this.Text = "ProcessRecorder";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem01;
+    }
+}
+
---
+++ ProcessRecorder/Form1.cs
@@ -0,0 +1,178 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProcessRecorder
+{
+    public partial class Form1 : Form
+    {
+        Logger logger = LogManager.GetCurrentClassLogger();
+
+        Process[] oldProcess = new Process[0];
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            logger.Info("Application init");
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                this.Hide();
+
+                logger.Info("Application hide");
+
+                return;
+
+            }
+            else if(e.CloseReason == CloseReason.ApplicationExitCall)
+            {
+                logger.Info("Application close");
+
+            }
+
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Show();
+                this.Focus();
+
+                logger.Info("Application show");
+
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Process[] nowProcess = Process.GetProcesses();
+
+            Process[] newProcess = nowProcess.Where(i => !oldProcess.Any(j => i.Id == j.Id && i.ProcessName == j.ProcessName)).ToArray();
+            Process[] abortProcess = oldProcess.Where(i => !nowProcess.Any(j => i.Id == j.Id && i.ProcessName == j.ProcessName)).ToArray();
+
+            if(newProcess.Length > 0)
+            {
+                foreach (var i in newProcess)
+                {
+                    try
+                    {
+                        if (i.Id == i.Parent().Id)
+                            logger.Info(string.Format($"Create, ID: {i.Id:00000},\tName: {i.ProcessName},\tLocation: {i.MainModule.FileName}"));
+                        else
+                            logger.Info(string.Format($"Create, ID: {i.Id:00000},\tParentID: {i.Parent().Id:00000},\tName: {i.ProcessName},\tLocation: {i.MainModule.FileName}"));
+
+                    }
+                    catch(Exception ex)
+                    {
+                        Debug.Print($"Error msg: {ex.Message}");
+                        logger.Info(string.Format("Create, ID: {0:00000},\tName: {1}", i.Id, i.ProcessName));
+
+                    }
+
+                }
+
+            }
+
+            if (abortProcess.Length > 0)
+            {
+                foreach (var i in abortProcess)
+                {
+                    try
+                    {
+                        logger.Info($"Abort, ID: {i.Id:00000},\tName: {i.ProcessName},\tLocation: {i.MainModule.FileName}");
+
+                    }
+                    catch
+                    {
+                        logger.Info($"Abort, ID: {i.Id:00000},\tName: {i.ProcessName}");
+
+                    }
+
+                }
+
+            }
+
+            if(newProcess.Length > 0 || abortProcess.Length > 0)
+                oldProcess = (Process[])nowProcess.Clone();
+
+        }
+
+        private void ToolStripMenuItem01_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            logger.Info("Application load");
+
+            oldProcess = Process.GetProcesses();
+
+            this.Hide();
+
+        }
+
+        protected override void SetVisibleCore(bool value)
+        {
+            if (!this.IsHandleCreated)
+            {
+                value = false;
+                CreateHandle();
+            }
+            base.SetVisibleCore(value);
+        }
+
+    }
+
+    public static class ProcessExtensions
+    {
+        private static string FindIndexedProcessName(int pid)
+        {
+            var processName = Process.GetProcessById(pid).ProcessName;
+            var processesByName = Process.GetProcessesByName(processName);
+            string processIndexdName = null;
+
+            for (var index = 0; index < processesByName.Length; index++)
+            {
+                processIndexdName = index == 0 ? processName : processName + "#" + index;
+                
+                var processId = new PerformanceCounter("Process", "ID Process", processIndexdName);
+
+                if ((int)processId.NextValue() == pid)
+                    return processIndexdName;
+            }
+
+            return processIndexdName;
+
+        }
+
+        private static Process FindPidFromIndexedProcessName(string indexedProcessName)
+        {
+            var parentId = new PerformanceCounter("Process", "Creating Process ID", indexedProcessName);
+            
+            return Process.GetProcessById((int)parentId.NextValue());
+
+        }
+
+        public static Process Parent(this Process process)
+            => FindPidFromIndexedProcessName(FindIndexedProcessName(process.Id));
+    }
+
+}
